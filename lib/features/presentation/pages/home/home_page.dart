9b945380074baf25ea5f0f4649959af49ed4e6ed
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-02-15 23:00:33 +0700

    p=fsfns

--- lib/features/presentation/pages/home/home_page.dart
+++ lib/features/presentation/pages/home/home_page.dart
@@ -21,6 +21,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final TextEditingController _searchController = TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  int _select = 0;
 
   List<Widget> listPersonActi = const [
     PersonAcityItem(
@@ -43,6 +44,24 @@ class _HomePageState extends State<HomePage> {
         image: 'assets/images/gmail.png',
         name: 'Nguyen Thanh Tung',
         isActive: true),
+  ];
+
+  List<Map<String, dynamic>> listOptionWidgets = [
+    {
+      'icon': Icons.message,
+      'text': 'Doan Chat',
+      'id': 0,
+    },
+    {
+      'icon': Icons.delete,
+      'text': 'Doan Chat Luu Tru',
+      'id': 1,
+    },
+    {
+      'icon': Icons.bar_chart_sharp,
+      'text': 'Thong Ke',
+      'id': 2,
+    }
   ];
   @override
   void initState() {
@@ -74,57 +93,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Colors.white,
-      drawer: Drawer(
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            centerTitle: false,
-            leading: Container(
-              height: 40.0,
-              width: 40.0,
-              margin: const EdgeInsets.all(5.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.red,
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                    currentUser.profileUrl == ""
-                        ? 'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
-                        : currentUser.profileUrl,
-                  ),
-                ),
-              ),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () => Navigator.pushNamed(
-                  context,
-                  PageConst.profilePage,
-                  arguments: widget.uid,
-                ),
-                icon: Icon(Icons.settings, color: textIconColorGray),
-              ),
-            ],
-            title: Text(
-              ' ${currentUser.name}',
-              style: headerText1.copyWith(fontSize: medumSizeText),
-            ),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(paddingAllWidget),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  children: [],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      drawer: _drawerWidget(currentUser),
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -206,9 +175,144 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Drawer _drawerWidget(UserEntity currentUser) {
+    return Drawer(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          centerTitle: false,
+          leading: Container(
+            height: 40.0,
+            width: 40.0,
+            margin: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  currentUser.profileUrl == ""
+                      ? 'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
+                      : currentUser.profileUrl,
+                ),
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () => Navigator.pushNamed(
+                context,
+                PageConst.profilePage,
+                arguments: widget.uid,
+              ),
+              icon: Icon(Icons.settings, color: textIconColorGray),
+            ),
+          ],
+          title: Text(
+            ' ${currentUser.name}',
+            style: headerText1.copyWith(fontSize: medumSizeText),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(paddingAllWidget - 5.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ...listOptionWidgets.map(
+                (e) => MainPageOptionItem(
+                  check: _select == e['id'],
+                  icon: Icon(
+                    e['icon'],
+                    color: textIconColorGray,
+                    size: 20.0,
+                  ),
+                  text: e['text'],
+                  select: () {
+                    setState(() {
+                      _select = e['id'];
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 15.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Cong dong',
+                    style: headerText2.copyWith(
+                      fontSize: medumSizeText,
+                      color: color747480,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Chinh Sua',
+                      style: headerText1.copyWith(
+                          fontSize: lowSizeText, color: blueColor),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
   // ListViewMain _bodyWidget() {
   //   return
   // }
+}
+
+class MainPageOptionItem extends StatelessWidget {
+  final bool check;
+  final Widget icon;
+  final String text;
+  final VoidCallback select;
+  const MainPageOptionItem({
+    super.key,
+    required this.check,
+    required this.icon,
+    required this.text,
+    required this.select,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: select,
+      child: Container(
+        padding: const EdgeInsets.all(5.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: check ? Colors.grey.withOpacity(0.1) : Colors.transparent,
+        ),
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(paddingAllWidget - 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey.withOpacity(0.2),
+              ),
+              child: icon,
+            ),
+            const SizedBox(width: 10.0),
+            Expanded(
+              child: Text(
+                text,
+                style: headerText2.copyWith(
+                  fontSize: medumSizeText,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class ChatItem extends StatelessWidget {
--- lib/features/presentation/pages/home/profile_page.dart
+++ lib/features/presentation/pages/home/profile_page.dart
@@ -1,13 +1,93 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:groupchat_clean_architecture/features/presentation/widgets/app_bar_widget.dart';
 import 'package:groupchat_clean_architecture/features/presentation/widgets/theme/style.dart';
+import 'package:groupchat_clean_architecture/features/presentation/widgets/theme/template.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-class ProfilePage extends StatelessWidget {
+import '../../../domain/entities/user_entity.dart';
+import '../../cubit/user/user_cubit.dart';
+
+class ProfilePage extends StatefulWidget {
   final String uid;
   const ProfilePage({super.key, required this.uid});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  List<Map<String, dynamic>> listOptions = [
+    {
+      'leadTitle': 'Che do toi',
+      'title': 'Tat',
+      'color': Colors.black,
+      'icon': Icons.dark_mode,
+    },
+    {
+      'leadTitle': 'Trang  thai hoat dong',
+      'title': 'Dang bat',
+      'color': const Color.fromARGB(255, 120, 251, 124),
+      'icon': Icons.local_activity_rounded,
+    },
+    {
+      'leadTitle': 'Ten nguoi dung',
+      'title': 'Minh Hung',
+      'color': Colors.red,
+      'icon': Icons.near_me,
+    }
+  ];
+
+  File? _file;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    try {
+      // ignore: deprecated_member_use
+      final pickedFile = await picker.getImage(source: ImageSource.gallery);
+      setState(() {
+        if (pickedFile != null) {
+          _file = File(pickedFile.path);
+
+          // StorageProviderRemoteDataSource.uploadFile(file: _image!)
+          //     .then((value) {
+          //   print("profileUrl");
+          //   setState(() {
+          //     _profileUrl = value;
+          //   });
+          // });
+        } else {
+          print('No image selected.');
+        }
+      });
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (context, userState) {
+        if (userState is UserLoaded) {
+          return _bodyWidget(context, userState.users);
+        }
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: CircularProgressIndicator(color: primaryColor),
+          ),
+        );
+      },
+    );
+  }
+
+  Scaffold _bodyWidget(BuildContext context, List<UserEntity> users) {
+    final currentUser =
+        users.firstWhere((element) => element.uid == widget.uid);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: appBarWidget(
@@ -18,6 +98,145 @@ class ProfilePage extends StatelessWidget {
         null,
         null,
         null,
+      ),
+      body: ListViewMain(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        currentUser.profileUrl == ""
+                            ? 'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
+                            : currentUser.profileUrl,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 60.0,
+                  top: 60.0,
+                  child: InkWell(
+                    onTap: () {
+                      getImage();
+                    },
+                    child: Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 5.0, color: Colors.white),
+                        color: Colors.grey.withOpacity(0.8),
+                      ),
+                      child: Icon(
+                        Icons.camera_alt_rounded,
+                        color: textIconColorGray,
+                        size: 16.0,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              currentUser.name,
+              style: headerText1.copyWith(fontSize: headerSizeText - 12),
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          ...listOptions.map(
+            (e) => OptionSettingItem(
+              icon: Icon(
+                e['icon'],
+                color: Colors.white,
+                size: 16.0,
+              ),
+              color: e['color'],
+              leadTitle: e['leadTitle'],
+              title: e['title'],
+              select: () {},
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: horizontalAllSize),
+            child: Text(
+              'Dich Vu',
+              style: headerText1.copyWith(
+                fontSize: medumSizeText,
+                color: color747480,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OptionSettingItem extends StatelessWidget {
+  final Color color;
+  final Widget icon;
+  final String leadTitle;
+  final String title;
+  final VoidCallback select;
+  const OptionSettingItem({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.leadTitle,
+    required this.title,
+    required this.select,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: horizontalAllSize, vertical: 10.0),
+      child: InkWell(
+        onTap: select,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: 35.0,
+              height: 35.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: color,
+              ),
+              child: icon,
+            ),
+            const SizedBox(width: 10.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  leadTitle,
+                  style: headerText2.copyWith(fontSize: medumSizeText),
+                ),
+                Text(
+                  title,
+                  style: headerText2.copyWith(fontSize: lowSizeText),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
