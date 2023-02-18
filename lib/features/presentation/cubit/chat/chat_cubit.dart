08aefe9df8fd0f5af0583e443eb532caf5090404
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-02-18 21:41:26 +0700

    fix

---
+++ lib/features/presentation/cubit/chat/chat_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:groupchat_clean_architecture/features/domain/entities/chat_entity.dart';
+
+import '../../../domain/use_cases/get_current_uid_usecase.dart';
+import '../../../domain/use_cases/is_sign_in_usecase.dart';
+import '../../../domain/use_cases/sign_out_usecase.dart';
+part 'chat_state.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  final IsSignInUseCase isSignInUseCase;
+  final SignOutUseCase signOutUseCase;
+  final GetCurrentUidUseCase getCurrentUidUseCase;
+
+  ChatCubit(
+      {required this.isSignInUseCase,
+      required this.signOutUseCase,
+      required this.getCurrentUidUseCase})
+      : super(ChatInitial());
+}
